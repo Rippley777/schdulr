f54a00dc8900fc1535d9aa91b73d0b3986589557
Rippley777 <ally.rippley@gmail.com>
2024-10-12 12:51:52 -0500

    add layout UI logic (#5)

--- src/components/page/header/index.tsx
+++ src/components/page/header/index.tsx
@@ -1,5 +1,34 @@
-const Header: React.FC = () => {
-  return <div>Header</div>;
+import reactLogo from "../../../assets/react.svg";
+import { PageConfig } from "../../../types";
+import siteConfigJson from "../../../site.config.json";
+
+type HeaderProps = {
+  fixed?: boolean;
+};
+
+const Header: React.FC<HeaderProps> = () => {
+  return (
+    <div className="w-full min-h-20 bg-gray-700 flex justify-between items-center px-5">
+      <div>
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </div>
+      <div className="flex">
+        {siteConfigJson.pages.map((page: PageConfig) => (
+          <HeaderLink href={page.path}>{page.title}</HeaderLink>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Header;
+
+type HeaderLinkProps = { href: string; children: React.ReactNode };
+
+const HeaderLink: React.FC<HeaderLinkProps> = ({ href, children }) => {
+  return (
+    <div className="mx-5">
+      <a href={href}>{children}</a>
+    </div>
+  );
+};
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,5 @@
+/* Configuration types */
+
 export type SiteConfig = {
   pages: PageConfig[];
 };
