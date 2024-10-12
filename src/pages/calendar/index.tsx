0299d9ca9905d9fc103601f65db20acecdcf48e8
Ally Rippley <ally.rippley@gmail.com>
2024-10-12 15:06:32 -0500

    add basis for calendar page

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import Page from "../../components/page";
+import "./calendar.css";
+import { filterDays, filterTimes } from "./utils";
 
 const CalendarPage: React.FC = () => {
-  return <Page>Calendar</Page>;
+  const [startDate, setStartDate] = useState<Date | null>(new Date());
+
+  return (
+    <Page bodyStyles="flex flex-col items-center">
+      <div className="w-[80vh]">
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          monthsShown={1}
+          filterTime={filterTimes}
+          filterDate={filterDays}
+          showTimeSelect
+          inline
+        />
+      </div>
+      {startDate && <div>{startDate.toLocaleDateString()}</div>}
+      {startDate && <div>{startDate.toLocaleTimeString()}</div>}
+    </Page>
+  );
 };
 
 export default CalendarPage;
