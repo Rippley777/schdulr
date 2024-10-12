2aa600205c5c9965c16698dc2a6f959a7fe89f5c
Rippley777 <ally.rippley@gmail.com>
2024-10-12 16:11:39 -0500

    add basis for calendar page (#6)

--- src/components/page/index.tsx
+++ src/components/page/index.tsx
@@ -1,23 +1,31 @@
-import { twMerge } from "tailwind-merge";
+import { ClassNameValue, twMerge } from "tailwind-merge";
 
 import Footer from "./footer";
 import Header from "./header";
 
 type PageProps = {
   children: React.ReactNode;
+  bodyStyles?: ClassNameValue;
   fixedFooter?: boolean;
   fixedHeader?: boolean;
 };
 
 const Page: React.FC<PageProps> = ({
   children,
+  bodyStyles = "",
   fixedFooter = false,
   fixedHeader = true,
 }) => {
   return (
     <div className="flex flex-col h-full">
       <Header fixed={fixedHeader} />
-      <div className={twMerge(`flex-1`, fixedFooter ? "overflow-scroll" : "")}>
+      <div
+        className={twMerge(
+          `flex-1`,
+          bodyStyles,
+          fixedFooter ? "overflow-scroll" : ""
+        )}
+      >
         {children}
       </div>
       <Footer />
